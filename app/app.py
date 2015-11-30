520fb2398e93a8744f55ecd92feec6a087922451
arpejupe <apj.pekkarinen@gmail.com>
2015-11-30 17:09:35 +0200

    Matkanaattori app can now take coordinates and send them to matka.fi API for xml

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
 import cherrypy
+import requests
 
-class HelloWorld(object):
+#for debugging
+from pprint import pprint
+
+class Matka(object):
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
+        return "Welcome using matkanaattori!"
+
+    @cherrypy.expose
+    def locate(self, start, destination):
+        # http://api.matka.fi/?a=3597369,6784330&b=3392009,6686355&user=matkanaattori&pass=ties532soa
+        response = requests.get("http://api.matka.fi/?a=" + start + "&b=" + destination + "&user=matkanaattori&pass=ties532soa")
+        xml = response.text
+        cherrypy.response.headers["Content-Type"] = "text/xml; charset=utf-8"
+        return xml
 
 if __name__ == '__main__':
-   cherrypy.quickstart(HelloWorld())
+    cherrypy.quickstart(Matka())
