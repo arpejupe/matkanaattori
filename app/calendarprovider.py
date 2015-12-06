2cffca9b091c3e0ce48ee757ae847ec1de20848c
Juha Moisio <juha.pa.moisio@student.jyu.fi>
2015-12-06 11:17:25 +0200

    memcached

--- app/calendarprovider.py
+++ app/calendarprovider.py
@@ -2,11 +2,21 @@ from requests import get
 from icalendar import Calendar
 from datetime import datetime
 import pytz
+import pylibmc
+
+cache = pylibmc.Client(["127.0.0.1"], binary=True, behaviors={"tcp_nodelay": True, "ketama": True})
+
+def getCalendar(url):
+    calendar = cache.get(url)
+    if calendar is None:
+        r = get(url, stream=True)
+        calendar = Calendar.from_ical(r.content)
+        cache.set(url, calendar, time=1800)
+    return calendar
 
 class UCalendar(Calendar):
     def __init__(self, icalurl):
-        r = get(icalurl, stream=True) ## this should get the calendar from the calendars cache service
-        self.cal = Calendar.from_ical(r.content)
+        self.cal = getCalendar(icalurl)
 
     def getNextEvent(self):
         now = datetime.now(pytz.utc)
@@ -20,8 +30,9 @@ class UCalendar(Calendar):
 
 if __name__ == '__main__':
     url = "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a"
-    cal = UCalendar(url)
-    nextEvent = cal.getNextEvent()
-    print(nextEvent.get("summary"))
-    print(nextEvent.get("location"))
-    print(nextEvent.get("dtstart").dt)
+    for x in range(0,5):
+        cal = UCalendar(url)
+        nextEvent = cal.getNextEvent()
+        print(nextEvent.get("summary"))
+        print(nextEvent.get("location"))
+        print(nextEvent.get("dtstart").dt)
