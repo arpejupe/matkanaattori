c4626112c53e86f69d21bd0fced070275cf60d62
Juha Moisio <juha.pa.moisio@student.jyu.fi>
2015-12-06 17:13:23 +0200

    tuki monelle kalenterille

--- app/calendarprovider.py
+++ app/calendarprovider.py
@@ -6,33 +6,54 @@ import pylibmc
 
 cache = pylibmc.Client(["127.0.0.1"], binary=True, behaviors={"tcp_nodelay": True, "ketama": True})
 
-def getCalendar(url):
-    calendar = cache.get(url)
-    if calendar is None:
-        r = get(url, stream=True)
-        calendar = Calendar.from_ical(r.content)
-        cache.set(url, calendar, time=1800)
-    return calendar
+class Events(object):
+    def __init__(self):
+        self.events = []
 
-class UCalendar(Calendar):
-    def __init__(self, icalurl):
-        self.cal = getCalendar(icalurl)
+    def addEvent(self, event):
+        self.events.append(event)
 
     def getNextEvent(self):
         now = datetime.now(pytz.utc)
-        iterEvents = iter(self.cal.walk('vevent'))
+        iterEvents = iter(self.events)
         nextEvent = next(iterEvents)
         for event in iterEvents:
-            start = event.get("dtstart").dt
-            if start > now and start < nextEvent.get("dtstart").dt:
+            start = event["start"]
+            if start > now and start < nextEvent["start"]:
                 nextEvent = event
         return nextEvent
 
+    def join(self, eventsObj):
+        self.events += eventsObj.events
+
+def getiCalEvents(url):
+    events = cache.get(url)
+    if events is None:
+        events = Events()
+        present = datetime.now(pytz.utc)
+        r = get(url, stream=True)
+        calendar = Calendar.from_ical(r.content)
+        for event in calendar.walk('vevent'):
+            start = event.get("dtstart").dt
+            location = event.get("location")
+            if start > present:
+                events.addEvent({"start": start,
+                                 "location": str(location)})
+        cache.set(url, events, time=1800)
+    return events
+
+def getEventsForAll(urls):
+    events = Events()
+    for url in urls:
+        events.join(getiCalEvents(url))
+    return events
+
 if __name__ == '__main__':
-    url = "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a"
+    calendars = ["https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
+            "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
+            "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
+            "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a"]
     for x in range(0,5):
-        cal = UCalendar(url)
-        nextEvent = cal.getNextEvent()
-        print(nextEvent.get("summary"))
-        print(nextEvent.get("location"))
-        print(nextEvent.get("dtstart").dt)
+        events = getEventsForAll(calendars)
+        nextEvent = events.getNextEvent()
+        print(nextEvent)
