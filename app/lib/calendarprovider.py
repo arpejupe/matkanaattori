35029bfd85216582232cba59342866cb6b99110b
Juha Moisio <juha.pa.moisio@student.jyu.fi>
2015-12-08 23:13:22 +0200

    calendarprovider update

--- app/lib/calendarprovider.py
+++ app/lib/calendarprovider.py
@@ -1,64 +1,52 @@
 from requests import get
 from icalendar import Calendar
 from datetime import datetime
+import pytz
 import pylibmc
 
 cache = pylibmc.Client(["127.0.0.1"], binary=True, behaviors={"tcp_nodelay": True, "ketama": True})
 
-class Events(object):
-    def __init__(self):
-        self.events = []
+def getCalendar(url):
+    request = get(url)
+    if request.status_code is 200:
+        return Calendar.from_ical(request.content)
+    return None
 
-    def addEvent(self, event):
-        self.events.append(event)
+def getCalendarEvents(url):
+    calendar = getCalendar(url)
+    events = calendar.walk("vevent")
+    return parseEvents(events)
 
-    def getNextEvent(self):
-        iterEvents = iter(self.events)
-        nextEvent = next(iterEvents)
-        if nextEvent:
-            now = datetime.now(nextEvent["start"].tzinfo)
-        for event in iterEvents:
-            start = event["start"]
-            if start > now and start < nextEvent["start"]:
-                nextEvent = event
-        return nextEvent
-
-    def join(self, eventsObj):
-        self.events += eventsObj.events
-
-def getiCalEvents(url):
-    url = str(url)
-    events = cache.get(url)
-    if events is None:
-        events = Events()
-        r = get(url, stream=True)
-        calendar = Calendar.from_ical(r.content)
-        iterEvents = iter(calendar.walk('vevent'))
-        nextEvent = next(iterEvents)
-        if nextEvent:
-            now = datetime.now(nextEvent["dtstart"].dt.tzinfo)
-        for event in calendar.walk('vevent'):
-            start = event.get("dtstart").dt
-            location = event.get("location")
-            # exclue past events
-            if start > now:
-                events.addEvent({"start": start,
-                                 "location": str(location)})
-        cache.set(url, events, time=1800) # cache expire time in seconds
-    return events
-
-def getEventsForAll(urls):
-    events = Events()
+def getNextEvent(*urls):
+    allevents = []
     for url in urls:
-        events.join(getiCalEvents(url))
-    return events
+        cachedEvents = cache.get(url)
+        if cachedEvents is None:
+            cachedEvents = getCalendarEvents(url)
+            # cache values: number of events, expire time in seconds
+            cache.set(url, cachedEvents[:10], 1800)
+        allevents += cachedEvents
+    currentTime = getCurrentTime()
+    for event in sorted(allevents, key=lambda event: event["startTime"]):
+        if event["startTime"] > currentTime:
+            return event
+    return None
+
+def parseEvents(events):
+    currentTime = getCurrentTime()
+    # parse past events and include only required data
+    events = [{"summary": event.get("summary").to_ical(),
+                "startTime": event.get("dtstart").dt,
+                "location": str(event.get("location"))} for event in events if event.get("dtstart").dt >= currentTime]
+    return sorted(events, key=lambda event: event["startTime"])
+
+def getCurrentTime():
+    return pytz.utc.localize(datetime.now())
 
 if __name__ == '__main__':
     calendars = ["https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
             "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
             "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a",
             "https://korppi.jyu.fi/calendar/ical/9e861b35b1d2cdb82d8feb805e64fd43decf4dfc0b3d5d1fc177114e634ede2a"]
-    for x in range(0,5):
-        events = getEventsForAll(calendars)
-        nextEvent = events.getNextEvent()
-        print(nextEvent)
+    nextEvent = getNextEvent(*calendars)
+    print(nextEvent)
