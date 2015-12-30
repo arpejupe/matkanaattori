ab13e4c5020d06403e05e7f603e99d2ea733e3bb
Juha Moisio <juha.pa.moisio@student.jyu.fi>
2015-12-30 15:37:57 +0200

    add download link for Firefox extension

--- src/application/controller/index.py
+++ src/application/controller/index.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os
 import cgi
 import cherrypy
 from config import constant
+from cherrypy.lib.static import serve_file
 
 __all__ = ['IndexController']
 
@@ -10,7 +12,7 @@ class IndexController(object):
     def __init__(self, controllers):
     	for key, value in controllers.items():
     		setattr(self, key, value)
-        
+
     @cherrypy.expose
     @cherrypy.tools.mako(filename="index.html")
     def index(self):
@@ -21,3 +23,9 @@ class IndexController(object):
             username = None
         return {"pointer": "index",
                 "user" : username}
+
+    @cherrypy.expose
+    def extension(self):
+        downloadPath = '/home/ubuntu/extension/matkanaattori_addon-1.0.0-fx+an.xpi'
+        filepath = os.path.abspath(downloadPath)
+        return serve_file(filepath, "application/x-download", "attachment")
